d37179c757e1024208a49cb0a0a73172a8775da6
kvxy <81992786+kvxy@users.noreply.github.com>
2022-10-31 18:55:51 -0700

    small changes (adding camera soon)

--- public/koxel.js
+++ public/koxel.js
@@ -43,7 +43,26 @@ function ClientEngine() {
 
 /** ./src/client/graphics/camera.js **/
 
+const Camera = (function()  {
 
+  function Camera() {
+    this.position = [0, 0, 0];
+    this.rotation = [0, 0, 0];
+
+    this.speed = 0.2;
+    this.sensitivity = 1;
+  }
+
+  // temp movement for camera
+  Camera.prototype.move = function(x, y, z) {
+    this.position[0] += x * this.speed;
+    this.position[1] += y * this.speed;
+    this.position[2] += z * this.speed;
+  };
+
+  return Camera;
+
+})();
 
 
 /** ./src/client/graphics/gpuResource.js **/
